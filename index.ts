81d4fd60a5bd1750a3bf1efea15ecaec6ae772ea
Tymoteusz Kiepura <kiepurek@gmail.com>
2022-05-19 10:03:25 +0200

    added all endpoints.

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import 'express-async-errors';
 import {handleError} from "./uttils/errors";
 import {adRecord} from "./records/ad.record";
 import rateLimit from "express-rate-limit";
+import {adRouter} from "./routers/ad.router";
 const app = express()
 
 app.use(cors({
@@ -15,10 +16,11 @@ app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }))
+
+app.use('/ad', adRouter);
+
 app.use(handleError)
-app.get('/test', async (req, res) => {
-    console.log(await adRecord.listChoosen('Karol'))
-})
+
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
 })
